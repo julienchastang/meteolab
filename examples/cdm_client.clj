2b66ef662b2d0971605942b05ccef4882c306c40
Julien Chastang <julien.c.chastang@gmail.com>
2012-02-17 23:17:15 -0700

    improvements in the CDM client API and the example

--- examples/cdm_client.clj
+++ examples/cdm_client.clj
@@ -3,23 +3,36 @@
         [incanter core charts])
   (:import [ucar.units UnitFormatManager]))
 
-;;(def catalog-uri "http://motherlode.ucar.edu/thredds/catalog/fmrc/NCEP/GFS/CONUS_80km/runs/catalog.xml")
-;;(def catalog-uri "http://motherlode.ucar.edu/thredds/catalog/fmrc/NCEP/GFS/Global_0p5deg/runs/catalog.xml")
-(def catalog-uri "http://motherlode.ucar.edu/thredds/catalog/fmrc/NCEP/NAM/CONUS_12km/runs/catalog.xml")
+(def catalogs
+  ["http://motherlode.ucar.edu/thredds/catalog/fmrc/NCEP/GFS/CONUS_80km/runs/catalog.xml"
+   "http://motherlode.ucar.edu/thredds/catalog/fmrc/NCEP/GFS/Global_0p5deg/runs/catalog.xml"
+   "http://motherlode.ucar.edu/thredds/catalog/fmrc/NCEP/NAM/CONUS_12km/runs/catalog.xml"
+   "http://motherlode.ucar.edu/thredds/catalog/fmrc/NCEP/RUC2/CONUS_13km/runs/catalog.xml"])
 
 (def v "Temperature_height_above_ground")
 
-(def ts-data
-  (unit-convert
-   (time-series (dataset-latest catalog-uri) v 40 -105.27 0)
-   "Fahrenheit"))
+(def models
+  (pmap #(unit-convert
+          (time-series
+           (dataset-latest %)
+           v [40 -105.27 0])
+          "Fahrenheit")
+        catalogs))
 
-(let [time (:time ts-data)
-      temperature (-> ts-data :data :vals)]
-  (view
-   (time-series-plot time
-                     temperature
-                     :title (-> ts-data :data :desc)
-                     :x-label "time"
-                     :y-label (-> ts-data :data :unit str)
-                     :legend true)))
+(def chart
+  (time-series-plot (:time (first models))
+                    (-> (first models) :data :vals)
+                    :title v
+                    :x-label "time"
+                    :y-label (-> (first models) :data :unit str)
+                    :legend true))
+
+;; (view chart)
+
+(doall
+ (map #(add-lines
+        chart
+        (:time %)
+        (-> % :data :vals)) (next models)))
+
+(view chart)
--- modules/meteolab-data/src/meteolab/cdm.clj
+++ modules/meteolab-data/src/meteolab/cdm.clj
@@ -5,56 +5,6 @@
            [ucar.units UnitFormatManager]
            [thredds.catalog InvCatalogFactory]))
 
-(defn metadata
-  "Get metadata associated with THREDDS URI"
-  [data-uri]
-  (with-open [gds (GridDataset/open data-uri)]
-    (let [vrs (.getDataVariables gds)
-          atbs (map (memfn getAttributes) vrs)
-          md (map (fn [x]
-                    (into {}
-                          (map (fn [y]
-                                 (vector
-                                  (keyword (.getName y))
-                                  (.getObject (.getValues y) 0))) x))) atbs)]
-      (zipmap
-       (map (memfn getName) vrs)
-       md))))
-
-(defn time-series
-  "Returns time series for data that can be read through the CDM API."
-  [data-uri v lat lon z]
-  (with-open [gds (GridDataset/open data-uri)]
-    (let [grid (.findGridByName gds v)
-          unit (.parse (UnitFormatManager/instance)
-                       (-> grid .getVariable .getUnitsString))
-          desc (-> grid .getVariable .getDescription)
-          gcs (.getCoordinateSystem grid)
-          dates (doall (map
-                        (memfn getTime)
-                        (-> gcs .getTimeAxis1D .getTimeDates)))
-          [x y]  (.findXYindexFromLatLon gcs lat lon (int-array 2))
-          d (doall (map
-                    #(-> (.readDataSlice grid % z y x) .get)
-                    (range (count dates))))]
-      {:time dates :data {:vals d :var v :unit unit :desc desc}})))
-
-(defn unit-convert
-  "Given a time series and a unit string, convert the time series to the unit.
-If the unit is not compatible, the original data is handed back"
-  [ts unit]
-  (let [from-unit (-> ts :data :unit)
-        to-unit (.parse (UnitFormatManager/instance) unit)]
-    (if (.isCompatible from-unit to-unit)
-      (assoc-in
-       (update-in ts [:data :vals]
-                  (fn [c]
-                    (map
-                     #(.convertTo from-unit (double  %) to-unit)
-                     c)))
-       [:data :unit] unit)
-      ts)))
-
 (defn- branch? [x]
   (if (instance? thredds.catalog.InvCatalogImpl x)
     true
@@ -73,12 +23,66 @@ If the unit is not compatible, the original data is handed back"
 (defn datasets
   "Given a THREDDS catalog, hands back a list of datasets as THREDDS URIs"
   [catalog-uri]
-  (map #(-> % access :ncdods)
+  (map #(zipmap [:name :uri]
+         (vector (.getName %)
+                        (-> % access :ncdods)))
        (let [cat (.readXML (InvCatalogFactory. "default" true) catalog-uri)]
          (sort-by millisec
                   (filter #(not (branch? %))
                           (tree-seq branch? #(.getDatasets %) cat))))))
 
-(defn dataset-latest [catalog-uri]
+(defn dataset-latest
   "Given a THREDDS catalog, hands back a list of datasets as THREDDS URIs"
+  [catalog-uri]
   (last (datasets catalog-uri)))
+
+(defn metadata
+  "Get metadata associated with THREDDS dataset"
+  [dataset]
+  (with-open [gds (GridDataset/open (:uri dataset))]
+    (let [vrs (.getDataVariables gds)
+          atbs (map (memfn getAttributes) vrs)
+          md (map (fn [x]
+                    (into {}
+                          (map (fn [y]
+                                 (vector
+                                  (keyword (.getName y))
+                                  (.getObject (.getValues y) 0))) x))) atbs)]
+      (zipmap
+       (map (memfn getName) vrs)
+       md))))
+
+(defn time-series
+  "Returns time series for data that can be read through the CDM API."
+  [dataset v [lat lon z]]
+  (with-open [gds (GridDataset/open (:uri dataset))]
+    (let [grid (.findGridByName gds v)
+          unit (.parse (UnitFormatManager/instance)
+                       (-> grid .getVariable .getUnitsString))
+          desc (-> grid .getVariable .getDescription)
+          gcs (.getCoordinateSystem grid)
+          dates (doall (map
+                        (memfn getTime)
+                        (-> gcs .getTimeAxis1D .getTimeDates)))
+          [x y]  (.findXYindexFromLatLon gcs lat lon (int-array 2))
+          d (doall (map
+                    #(-> (.readDataSlice grid % z y x) .get)
+                    (range (count dates))))]
+      {:name (:name dataset) :time dates
+       :data {:vals d :var v :unit unit :desc desc}})))
+
+(defn unit-convert
+  "Given a time series and a unit string, convert the time series to the unit.
+If the unit is not compatible, the original data is handed back"
+  [ts unit]
+  (let [from-unit (-> ts :data :unit)
+        to-unit (.parse (UnitFormatManager/instance) unit)]
+    (if (.isCompatible from-unit to-unit)
+      (assoc-in
+       (update-in ts [:data :vals]
+                  (fn [c]
+                    (map
+                     #(.convertTo from-unit (double  %) to-unit)
+                     c)))
+       [:data :unit] unit)
+      ts)))
